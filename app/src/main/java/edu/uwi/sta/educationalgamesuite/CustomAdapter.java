449a817df0a7dfa289453feb46f8fff3c382e246
logan20 <kwasi_edwards2@hotmail.com>
2016-04-03 12:24:21 -0400

    Added a couple activities, added custom adapteretc for games, Please give feedback asap

---
+++ app/src/main/java/edu/uwi/sta/educationalgamesuite/CustomAdapter.java
@@ -0,0 +1,76 @@
+package edu.uwi.sta.educationalgamesuite;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.awt.font.TextAttribute;
+
+/**
+ * Created by kwasi on 4/3/2016.
+ */
+public class CustomAdapter extends BaseAdapter{
+    String [] result;
+    Context context;
+    int[] img;
+    private static LayoutInflater inflater;
+    public CustomAdapter(MainActivity m, String[] strList, int[] picList){
+        context=m;
+        result = strList;
+        img=picList;
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public int getCount() {
+        return result.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        SingleRow sr = new SingleRow();
+        View rowView = inflater.inflate(R.layout.game_item,null);
+        sr.tv=(TextView) rowView.findViewById(R.id.txtViewTxt);
+        sr.iv= (ImageView) rowView.findViewById(R.id.imgViewImg);
+
+        sr.tv.setText(result[position]);
+        sr.iv.setImageResource(img[position]);
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (position){
+                    case 0:
+                        Log.d("ITEM CLICKED","Play sudoku");
+                        context.startActivity(new Intent(context,PlaySudokuActivity.class));
+                        break;
+                    default:
+                        Toast.makeText(context,"You have not coded this ish yet son",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        return rowView;
+    }
+
+    public class SingleRow{
+        TextView tv;
+        ImageView iv;
+    }
+
+}
--- app/src/main/java/edu/uwi/sta/educationalgamesuite/MainActivity.java
+++ app/src/main/java/edu/uwi/sta/educationalgamesuite/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.uwi.sta.educationalgamesuite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.client.AuthData;
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     Firebase firebaseRef,userRef;
     AuthData authData ;
     Map<String,Object> user;
+    public static int[] gameIcons={R.drawable.sudoku,R.drawable.another_game,R.drawable.another_game};
+    public static String[] gameStrings = {"Sudoku","Game 2","Game 3"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if (user.get("uid").equals(authData.getUid())) {
                     String name = user.get("name").toString();
-                    tv.setText("WELCOME " + name);
+                    if (name!=null)
+                        tv.setText(name);
                 } else {
                     tv.setText("WELCOME");
                     user = null;
@@ -64,7 +72,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //CURRENT USER LOGGED IN DATA IS NOW STORED IN user and can be manipulated
-        //manipulate the map data with user data and to updata firebase use
+        //manipulate the map data with user data and to update firebase use
         //userRef.child(user.get("uid")).setValue(map);
+
+        //set on click listener for image button to go to profile when clicked
+        (findViewById(R.id.imgBtnProfile)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+            }
+        });
+
+        //get the textview and assign the custom adapter
+        ((ListView) findViewById(R.id.lvGameList)).setAdapter(new CustomAdapter(this,gameStrings,gameIcons));
     }
 }
