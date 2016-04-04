59af256e0388c18796098aa35afda17b3650ff68
logan20 <kwasi_edwards2@hotmail.com>
2016-04-04 11:28:33 -0400

    done the base of the puzzle etc.. check the whatsapp group for what i did

--- app/src/main/java/edu/uwi/sta/educationalgamesuite/PlaySudokuActivity.java
+++ app/src/main/java/edu/uwi/sta/educationalgamesuite/PlaySudokuActivity.java
@@ -1,13 +1,65 @@
 package edu.uwi.sta.educationalgamesuite;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlaySudokuActivity extends AppCompatActivity {
+    protected Sudoku sudoku;
+    private GridView grid;
+    private int[] drawables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_sudoku_acivity);
+        sudoku = new Sudoku();
+        final int[] gameBoard = sudoku.getConvertedBoard();
+        grid = (GridView)findViewById(R.id.sudokuGrid);
+        setDrawables();
+        grid.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return gameBoard.length;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ImageView imageView;
+                if (convertView==null){
+                    imageView = new ImageView(getApplicationContext());
+                    imageView.setLayoutParams(new GridView.LayoutParams(60, 60));
+                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    imageView.setPadding(8, 8, 8, 8);
+                }
+                else{
+                    imageView = (ImageView)convertView;
+                }
+
+                imageView.setImageResource(drawables[gameBoard[position]-1]);
+                return imageView;
+            }
+        });
+    }
+
+    private void setDrawables() {
+        drawables = new int[]{R.drawable.one,R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five,
+                                R.drawable.six,R.drawable.seven, R.drawable.eight, R.drawable.nine};
     }
 }
---
+++ app/src/main/java/edu/uwi/sta/educationalgamesuite/Sudoku.java
@@ -0,0 +1,86 @@
+package edu.uwi.sta.educationalgamesuite;
+
+import android.util.Log;
+
+/**
+ * Created by kwasi on 4/3/2016.
+ */
+public class Sudoku {
+    private int[][] board;
+    private int[]solutions;
+    private int[] convertedBoard;
+    private final int SUDOKUSIZE = 3;
+    
+    public Sudoku(){
+        genSolutions();
+        genBoard();
+        printBoard();
+        randomise();
+        System.out.println("*******************");
+        printBoard();
+        setConvertedBoard();
+    }
+
+    public int[] getConvertedBoard() {
+        return convertedBoard;
+    }
+
+    private void setConvertedBoard() {
+        for (int a=0;a<SUDOKUSIZE*SUDOKUSIZE;a++){
+            for (int b=0;b<SUDOKUSIZE*SUDOKUSIZE;b++){
+                convertedBoard[(a*9)+b]=board[a][b];
+            }
+            System.out.print('\n');
+        }
+    }
+
+    private void randomise() {
+        for (int a=0;a<SUDOKUSIZE;a++){ //randomise rows
+            int row1= (int) (System.nanoTime()%3), row2=(row1+1)%3;
+            int [] tempRow = board[a*SUDOKUSIZE+row1];
+
+            board[a*SUDOKUSIZE+row1] = board[a*SUDOKUSIZE+row2];
+            board[a*SUDOKUSIZE+row2]=tempRow;
+        }
+    }
+
+    private void printBoard() {
+        for (int a=0;a<SUDOKUSIZE*SUDOKUSIZE;a++){
+            for (int b=0;b<SUDOKUSIZE*SUDOKUSIZE;b++){
+                System.out.print(board[a][b]+ " ");
+            }
+            System.out.print('\n');
+        }
+    }
+
+    private void genSolutions() {
+        solutions = new int[]{  3,2,9,6,5,7,8,4,1,
+                                7,4,5,8,3,1,2,9,6,
+                                6,1,8,2,4,9,3,7,5,
+
+                                1,9,3,4,6,8,5,2,7,
+                                2,7,6,1,9,5,4,8,3,
+                                8,5,4,3,7,2,6,1,9,
+
+                                4,3,2,7,1,6,9,5,8,
+                                5,8,7,9,2,3,1,6,4,
+                                9,6,1,5,8,4,7,3,2};//permutate from the groups to generate board
+    }
+
+    private void genBoard() {
+        board = new int[9][9];
+        convertedBoard = new int[81];
+        for (int a=0;a<SUDOKUSIZE*SUDOKUSIZE;a++){
+            for (int b=0;b<SUDOKUSIZE*SUDOKUSIZE;b++){
+                board[a][b] = getNumber(a,b);
+            }
+        }
+    }
+
+    private int getNumber(int a, int b) {
+        return solutions[a*SUDOKUSIZE*SUDOKUSIZE+b];
+    }
+    public int[][] getBoard(){
+        return board;
+    }
+}
